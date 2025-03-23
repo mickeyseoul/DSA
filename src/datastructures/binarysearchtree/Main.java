da9de21bf8761708312795bab4760ed224f7f7ea
mickeyseoul <flotdejoie@gmail.com>
2025-03-23 12:30:10 +0900

    Section 13: Trees

--- src/datastructures/binarysearchtree/Main.java
+++ src/datastructures/binarysearchtree/Main.java
@@ -7,6 +7,24 @@ public class Main {
 		BinarySearchTree myBST = new BinarySearchTree();
 		
 		System.out.println("root = " + myBST.root);
+		
+		
+		// insert()
+		myBST.insert(47);
+		myBST.insert(21);
+		myBST.insert(76);
+		myBST.insert(18);
+		myBST.insert(52);
+		myBST.insert(82);
+		
+		myBST.insert(27);
+		
+		System.out.println(myBST.root.left.right.value);
+		
+		// contains()
+		System.out.println(myBST.contains(27));
+		System.out.println(myBST.contains(17));
+		
 
 	}
 
