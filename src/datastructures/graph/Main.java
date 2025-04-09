9c8b8165067bc96c3928f92dab2709e5b9459dfb
mickeyseoul <flotdejoie@gmail.com>
2025-04-10 07:59:04 +0900

    Section 19: Graphs

---
+++ src/datastructures/graph/Main.java
@@ -0,0 +1,18 @@
+package datastructures.graph;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Graph myGraph = new Graph();
+		
+		myGraph.addVertex("A");
+		myGraph.addVertex("B");
+		
+		myGraph.addEdge("A", "B");
+		
+		myGraph.printGraph();
+
+	}
+
+}
